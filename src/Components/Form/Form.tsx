381ee6497e5c3de28cb9ba477d3c89d7bb791d1f
Santiago Álvarez Eraso <dg.santiago.alvarez@gmail.com>
2023-08-28 00:02:16 -0500

    Edición de estilos home

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,66 +1,66 @@
 import { useAppContext } from '@/context/context'
 import React, { FC, FormEvent, useEffect, useRef, useState } from 'react'
+import Search from '../icons/Search';
 
-interface Form {    
-    fetchNewCharacter: () => void;  
+interface Form {
+    fetchNewCharacter: () => void;
     loading: boolean;
-    currentCharacter: string 
+    currentCharacter: string
 }
 
-const Form : FC<Form> = ({fetchNewCharacter, loading, currentCharacter}) => {
+const Form: FC<Form> = ({ fetchNewCharacter, loading, currentCharacter }) => {
 
-    const {typeNewCharacter, characterForSearch, resetPagination} = useAppContext()
+    const { typeNewCharacter, characterForSearch, resetPagination } = useAppContext()
     const [inputError, setInputError] = useState('')
-    const inputRef = useRef<HTMLInputElement>(null)    
+    const inputRef = useRef<HTMLInputElement>(null)
     const MINIMUM_OF_SEARCH = 2
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
-        resetPagination()  
-        if((characterForSearch.length >= MINIMUM_OF_SEARCH) && (characterForSearch !== currentCharacter)){
+        resetPagination()
+        if ((characterForSearch.length >= MINIMUM_OF_SEARCH) && (characterForSearch !== currentCharacter)) {
             fetchNewCharacter()
-        }  
-        if(characterForSearch.length < MINIMUM_OF_SEARCH){
+        }
+        if (characterForSearch.length < MINIMUM_OF_SEARCH) {
             setInputError('Debes escribir 2 caracteres como mínimo'), inputRef.current?.focus()
         }
     }
 
     const validateInputError = () => {
         return !!inputError.length && (characterForSearch.length < MINIMUM_OF_SEARCH)
-    } 
+    }
 
     useEffect(() => {
-        if(inputRef.current){
+        if (inputRef.current) {
             inputRef.current.focus()
         }
-    },[])   
+    }, [])
 
-   
     return (
         <form
-            className='w-full  px-7 pb-6 sm:w-3/4 dm:w-3/5 lg:w-2/4' 
-            onSubmit={(e) => 
-            handleSubmit(e)}>
-            <div className='flex gap-2 justify-center'>
-                <input
-                    className= {`basis-3/4 border-slate-600 border-2 px-5 py-2 rounded-full bg-slate-600 text-slate-200 focus:outline-none ${validateInputError() ? 'focus:border-orange-500' : 'focus:border-cyan-600'} focus:text-slate-200`}
-                    type='text'
-                    placeholder='Escribe su nombre'
-                    onChange={(e) => typeNewCharacter(e.target.value)}
-                    value={characterForSearch}
-                    ref={inputRef}
-                />
-                <button  
-                    className='basis-2/6 rounded-full grow-0 bg-cyan-600 py-2 text-slate-200 disabled:bg-slate-600 hover:bg-cyan-500 transition-all' 
-                    type='submit'
-                >{loading ? 'Buscando...' : 'Buscar'}
+            className='w-full flex gap-2 justify-center px-7 pb-6 sm:w-3/5 lg:w-2/4 xl:w-full xl:p-0'
+            onSubmit={(e) =>
+                handleSubmit(e)}>            
+                <div className='sm:basis-3/4'>
+                    <input
+                        className={`h-12 w-full  border-slate-600 border-2 px-5 py-2 rounded-full bg-slate-600 text-slate-200 focus:outline-none ${validateInputError() ? 'focus:border-orange-500' : 'focus:border-cyan-600'} focus:text-slate-200`}
+                        type='text'
+                        placeholder='Escribe su nombre'
+                        onChange={(e) => typeNewCharacter(e.target.value)}
+                        value={characterForSearch}
+                        ref={inputRef}
+                    />
+                    {validateInputError()
+                        ? <p className=' w-full text-xs pl-6 pt-2 text-orange-500'>{inputError}</p>
+                        : <p className='w-full text-xs pl-6 pt-2 text-slate-400'>*2 caracteres como mínimo</p>
+                    }
+                </div>
+                <button
+                    className='flex justify-center items-center w-12 h-12 sm:basis-2/6 rounded-full grow-0 bg-cyan-600 py-2 px-2 text-slate-200 disabled:bg-slate-600 hover:bg-cyan-500 transition-all'
+                    type='submit'>
+                    <p className='hidden sm:block'>{loading ? 'Buscando...' : 'Buscar'}</p>
+                    {loading ? <p className='w-6 sm:hidden'>...</p> : <Search className='w-6 sm:hidden' />}
                 </button>
-            </div>            
-            {validateInputError()
-            ?  <p className='text-xs pl-6 pt-2 text-orange-500'>{inputError}</p>
-            :  <p className='text-xs pl-6 pt-2 text-slate-400'>*2 caracteres como mínimo</p>
-            }
-            
         </form>
     )
 }
--- src/Components/Wellcome/Wellcome.tsx
+++ src/Components/Wellcome/Wellcome.tsx
@@ -7,26 +7,28 @@ import Image from 'next/image';
 import Spinner from '../Spinner/Spinner';
 
 const Wellcome = () => {
-    const { fetchCharacter, dataForRender, generalInformation, error, loading, variables} = useCharacter()
+    const { fetchCharacter, dataForRender, generalInformation, error, loading, variables } = useCharacter()
     const { characterForSearch, firstPage } = useAppContext()
 
     return (
-        <>
-            <header className='flex flex-col justify-center items-center w-full gap-3 bg-slate-800 '>
-                <div className='w-3/4 h-32 relative mt-10'>
-                    <Image className=' object-contain' src='/Rick-And-Morty-Logo.png' alt='Rick-And-Morty-Logo' fill sizes='700vw, (min-width: 768px) 40vw' priority />
+        <>            
+            <header className={`${generalInformation === undefined ? 'h-screen' : 'h-64 xl:h-44 xl:flex-row'} transition-all flex flex-col justify-center items-center w-full gap-3 bg-slate-800 `}>                
+                <div className={`w-full h-28 relative mt-10 xl:my-6 xl:w-2/5`}>
+                    <Image className=' object-contain px-6 xl:px-0' src='/Rick-And-Morty-Logo.png' alt='Rick-And-Morty-Logo' fill sizes='700vw, (min-width: 768px) 40vw' priority />
                 </div>
-                <h1 className=' text-xl font-light text-cyan-500 '>Busca tu personaje favorito</h1>
-                <Form
-                    fetchNewCharacter={() => fetchCharacter(characterForSearch, firstPage)}   
-                    loading={loading}  
-                    currentCharacter={variables?.nameToSearch}               
-                />
+                <div className='w-full flex flex-col justify-center items-center gap-3 xl:w-2/5'>
+                    <h1 className='text-xl font-light text-cyan-500 xl:self-start xl:pl-5'>Busca tu personaje favorito</h1>
+                    <Form
+                        fetchNewCharacter={() => fetchCharacter(characterForSearch, firstPage)}
+                        loading={loading}
+                        currentCharacter={variables?.nameToSearch}
+                    />
+                </div>
             </header>
-            <main className='flex flex-col justify-center items-center w-full p-7'>
+            <main className={`${generalInformation === undefined ? 'hidden' : 'block'} flex flex-col justify-center items-center w-full p-7`}>
                 <div className='w-full flex items-center justify-center' id={variables?.nameToSearch} >
-                    {loading  && <div className=' xl:pt-20'><Spinner /></div> }
-                    {(!loading && !!dataForRender.length) && <CharacterList data={dataForRender}/>}
+                    {loading && <div className=' xl:pt-20'><Spinner /></div>}
+                    {(!loading && !!dataForRender.length) && <CharacterList data={dataForRender} />}
                     {error && <p>Error : {error.message}</p>}
                     {(!loading && generalInformation?.count === null) && <p className='self-center'>No encontramos el personaje buscabas</p>}
                 </div>
